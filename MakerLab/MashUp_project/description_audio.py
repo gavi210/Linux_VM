5e6114fe1a95a137f6c19c65e90602285e2b8a74
Rigoni Riccardo (Student Com19) <rccrd.rigoni@gmail.com>
2020-05-15 17:59:57 +0200

    description audio

--- MakerLab/MashUp_project/description_audio.py
+++ MakerLab/MashUp_project/description_audio.py
@@ -1,6 +1,7 @@
 # this class contains the methods for asking Google speech service to play the instructions audio
 from pygame import mixer
 import os
+from gtts import gTTS
 
 def play_instructions():
 
@@ -18,3 +19,14 @@ def play_instructions():
 
   #change back the working directory
   os.chdir('.')
+
+#function for creating the descrption file audio
+def create_description_audio():
+    os.chdir('audio')
+
+    text = 'Pull the joystick UP for the weather conditions under the ISS space station. Pull down for see the magic!'
+    text_audio = gTTS(text=text, lang='en')
+    text_audio.save('functionalities.mp3')
+    os.chdir('.')
+
+#create_description_audio()
